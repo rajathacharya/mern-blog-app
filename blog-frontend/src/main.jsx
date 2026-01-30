0573d47650d70e496381e80efd8b7edab69a9d9a
Rajath Acharya <rajath.g.achari@gmail.com>
2026-01-30 12:45:42 +0530

    Fixed Signup import path case issue for Vercel

--- blog-frontend/src/main.jsx
+++ blog-frontend/src/main.jsx
@@ -30,7 +30,7 @@ import App from "./App.jsx";
 import ProfilePage from "./pages/ProfilePage.jsx";
 import Home from "./components/Home.jsx";
 import Login from "./components/Login.jsx";
-import Signup from "./components/Signup.jsx";
+import Signup from "./components/SignUp.jsx";
 import AuthProvider from "./context/AuthContext.jsx";
 
 createRoot(document.getElementById("root")).render(
